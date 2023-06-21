7b99ec105bccfa6d49f311c2067c1b6ee8f8abb6
josephsenzota1999 <josephsenzota1999@gmail.com>
2023-06-21 08:57:19 +0100

    3 commit

--- app/src/main/java/com/example/expensemanager/AddExpenseActivity.java
+++ app/src/main/java/com/example/expensemanager/AddExpenseActivity.java
@@ -13,7 +13,8 @@ import android.widget.AdapterView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import com.example.expensemanager.databinding.ActivityAddExpenseBinding;
+import com.example.expensestracker.R;
+import com.example.expensestracker.databinding.ActivityAddExpenseBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -22,161 +23,151 @@ import java.util.Date;
 import java.util.UUID;
 
 public class AddExpenseActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+
     ActivityAddExpenseBinding binding;
     private String type;
     private String items;
     private ExpenseModel expenseModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding=ActivityAddExpenseBinding.inflate(getLayoutInflater());
+        binding = ActivityAddExpenseBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         Spinner category = findViewById(R.id.category);
         category.setOnItemSelectedListener(this);
 
-        type=getIntent().getStringExtra("type");
-        expenseModel=(ExpenseModel) getIntent().getSerializableExtra("model");
+        type = getIntent().getStringExtra("type");
+        expenseModel = (ExpenseModel) getIntent().getSerializableExtra("model");
 
-        if (type==null){
-            type=expenseModel.getType();
+        if (type == null) {
+            type = expenseModel.getType();
             binding.amount.setText(String.valueOf(expenseModel.getAmount()));
-           //binding.category.setText(expenseModel.getCategory());
             binding.note.setText(expenseModel.getNote());
         }
 
-        if (type.equals("Income")){
+        if (type.equals("Income")) {
             binding.incomeRadio.setChecked(true);
-        }else {
+        } else {
             binding.expenseRadio.setChecked(true);
         }
 
         binding.incomeRadio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                type="Income";
-            }
-        });
-        binding.expenseRadio.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                type="Expense";
+                type = "Income";
             }
         });
 
+        binding.expenseRadio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                type = "Expense";
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater=getMenuInflater();
-        if (expenseModel==null){
-            menuInflater.inflate(R.menu.add_menu,menu);
-        }else {
-            menuInflater.inflate(R.menu.update_menu,menu);
+        MenuInflater menuInflater = getMenuInflater();
+        if (expenseModel == null) {
+            menuInflater.inflate(R.menu.add_menu, menu);
+        } else {
+            menuInflater.inflate(R.menu.update_menu, menu);
         }
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id=item.getItemId();
-        if (id==R.id.saveExpense){
-            if (type!=null){
-                createExpense();
-            }else {
-                updateExpense();
+        int id = item.getItemId();
+
+        if (id == R.id.saveExpense) {
+            if (type != null) {
+                createOrUpdateExpense(); // Combined the logic for creating and updating expense
             }
             return true;
         }
-        if (id==R.id.deleteExpense){
+        if (id == R.id.deleteExpense) {
             deleteExpense();
+            return true;
         }
-        return false;
+        return super.onOptionsItemSelected(item);
     }
 
     private void deleteExpense() {
-        FirebaseFirestore
-                .getInstance()
-                .collection("expenses")
-                .document(expenseModel.getExpenseId())
-                .delete();
-        finish();
+        if (expenseModel != null) {
+            FirebaseFirestore
+                    .getInstance()
+                    .collection("expenses")
+                    .document(expenseModel.getExpenseId())
+                    .delete()
+                    .addOnSuccessListener(aVoid -> {
+                        Toast.makeText(AddExpenseActivity.this, "Expense deleted", Toast.LENGTH_SHORT).show();
+                        finish();
+                    })
+                    .addOnFailureListener(e -> {
+                        Toast.makeText(AddExpenseActivity.this, "Failed to delete expense", Toast.LENGTH_SHORT).show();
+                        Log.e("DeleteExpense", "Failed to delete expense", e);
+                    });
+        }
     }
 
-    private void createExpense() {
-        Calendar calendar = Calendar.getInstance();
-        Date currentDate = calendar.getTime();
-        String expenseId= UUID.randomUUID().toString();
-        String amount=binding.amount.getText().toString();
-        String note=binding.note.getText().toString();
-//         String category = binding.items.getText().toString();
-        expenseModel.setTime(currentDate.getTime());
-
-        boolean incomeChecked=binding.incomeRadio.isChecked();
-        if (incomeChecked){
-            type="Income";
-        }else {
-            type="Expense";
+    private void createOrUpdateExpense() {
+        String expenseId;
+        if (expenseModel == null) {
+            expenseId = UUID.randomUUID().toString();
+        } else {
+            expenseId = expenseModel.getExpenseId();
         }
 
-        if (amount.trim().length()==0){
+        String amount = binding.amount.getText().toString();
+        String note = binding.note.getText().toString();
+
+        if (amount.trim().isEmpty()) {
             binding.amount.setError("Empty");
             return;
         }
-        ExpenseModel expenseModel=new ExpenseModel(expenseId,note,items,type,Long.parseLong(amount), Calendar.getInstance().getTimeInMillis(),
-                FirebaseAuth.getInstance().getUid());
+
+        long currentTime = Calendar.getInstance().getTimeInMillis();
+        ExpenseModel model = new ExpenseModel(
+                expenseId,
+                note,
+                items,
+                type,
+                Long.parseLong(amount),
+                currentTime,
+                FirebaseAuth.getInstance().getUid()
+        );
 
         FirebaseFirestore
                 .getInstance()
                 .collection("expenses")
                 .document(expenseId)
-                .set(expenseModel);
-        finish();
-
-    }
-    private void updateExpense() {
-
-        String expenseId= expenseModel.getExpenseId();
-        String amount=binding.amount.getText().toString();
-        String note=binding.note.getText().toString();
-//       String category=binding.category.getText().toString();
-        Calendar calendar = Calendar.getInstance();
-        Date currentDate = calendar.getTime();
-
-        // Set the current date to the model
-        expenseModel.setTime(currentDate.getTime());
-        boolean incomeChecked=binding.incomeRadio.isChecked();
-        if (incomeChecked){
-            type="Income";
-        }else {
-            type="Expense";
-        }
-
-        if (amount.trim().length()==0){
-            binding.amount.setError("Empty");
-            return;
-        }
-        ExpenseModel model=new ExpenseModel(expenseId,note,items,type,Long.parseLong(amount),expenseModel.getTime() ,
-                FirebaseAuth.getInstance().getUid());
-
-        FirebaseFirestore
-                .getInstance()
-                .collection("expenses")
-                .document(expenseId)
-                .set(model);
-        finish();
-
+                .set(model)
+                .addOnSuccessListener(aVoid -> {
+                    if (expenseModel == null) {
+                        Toast.makeText(AddExpenseActivity.this, "Expense added", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(AddExpenseActivity.this, "Expense updated", Toast.LENGTH_SHORT).show();
+                    }
+                    finish();
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(AddExpenseActivity.this, "Failed to save expense", Toast.LENGTH_SHORT).show();
+                    Log.e("SaveExpense", "Failed to save expense", e);
+                });
     }
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-
         items = adapterView.getItemAtPosition(i).toString();
-        // Toast.makeText(adapterView.getContext(),"Selected "+item, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
-
+        // No implementation needed
     }
 }
